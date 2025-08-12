3ac01c0b67a8d59d986d2fd448eb4d696fb20f98
Igors Koptevs <igors.koptevs@gmail.com>
2025-08-12 08:50:08 +0300

    Edited header.php pattern

--- patterns/header.php
+++ patterns/header.php
@@ -6,13 +6,12 @@
  * Keywords: header
  * Block Types: core/template-part/header
  */
-?>
-<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"20px","bottom":"20px","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"margin":{"top":"0","bottom":"0"}},"border":{"bottom":{"color":"#8e8e8e","width":"1px"},"top":[],"right":[],"left":[]}},"backgroundColor":"white-text-color","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide has-white-text-color-background-color has-background" id="sticky-header" style="border-bottom-color:#8e8e8e;border-bottom-width:1px;margin-top:0;margin-bottom:0;padding-top:20px;padding-right:var(--wp--preset--spacing--20);padding-bottom:20px;padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+?><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"20px","bottom":"20px","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"margin":{"top":"0","bottom":"0"}},"border":{"bottom":{"color":"#8e8e8e","width":"1px"},"top":[],"right":[],"left":[]}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" id="sticky-header" style="border-bottom-color:#8e8e8e;border-bottom-width:1px;margin-top:0;margin-bottom:0;padding-top:20px;padding-right:var(--wp--preset--spacing--20);padding-bottom:20px;padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignwide"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:site-title {"level":0,"style":{"spacing":{"margin":{"right":"var:preset|spacing|20"}}}} /-->
 
-<!-- wp:navigation {"ref":335,"style":{"spacing":{"blockGap":"35px"}},"fontFamily":"body"} /--></div>
+<!-- wp:navigation {"ref":335,"style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontFamily":"body"} /--></div>
 <!-- /wp:group -->
 
 <!-- wp:buttons -->
