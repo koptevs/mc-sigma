cd6769c96983c469b4864e47e24ce994167b1816
Igors Koptevs <igors.koptevs@gmail.com>
2025-08-13 15:48:49 +0300

    Tuned build funcs

--- src/build/dist-build.js
+++ src/build/dist-build.js
@@ -4,16 +4,8 @@ const ignore = [
 	'dist',
 	'node_modules',
 	'src',
-	'vendor',
-	'composer.json',
-	'composer.lock',
 	'package.json',
 	'package-lock.json',
-	'phpcs.xml.dist',
-	'phpmd.baseline.xml',
-	'phpmd.xml',
-	'phpstan-baseline.neon',
-	'phpstan.neon.dist',
 ];
 
 utils.copyDir( './', `./dist/${ pkg.name }-${ pkg.version }`, ignore );
